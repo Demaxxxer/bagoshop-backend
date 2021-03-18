5258016950ef376fe06894c7895ae53190395988
rostakillnutej <rostakillnutej@gmail.com>
2021-03-18 16:43:59 +0100

    upravené hostovaní statického adresáře

--- server.js
+++ server.js
@@ -5,28 +5,22 @@ const path = require('path');
 const helmet = require("helmet");
 const router = require('./router.js');
 
-
 const port = 8080;
 const app = express();
-
-
-//console.log(__dirname);
-
-//app.use(express.static('uploads'));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
   extended: true
 }));
 
-
 app.use(cookieParser());
 app.use('/', router);
+
+app.use('/', express.static('public'));
 
 app.use('/api/uploads/', express.static('uploads'));
 
 /* Errory jsou zachyceny zde */
-/*
 app.use((req, res, next) => {
   res.status(404);
 });
@@ -34,10 +28,8 @@ app.use((err, req, res, next) => {
   console.error(err.stack);
   res.status(500);
 });
-*/
+
 
 app.listen(port, () => {
-  /*
   console.log(`Valí na portu: ${port}`)
-  */
 })
